4588ab3b1af8cc65f37f05f52d7b0e323c0dac96
sc20ab2 <sc20ab2@leeds.ac.uk>
2022-02-15 17:51:18 +0000

    Linking the database together

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'massi-very-secret-secret'
+SECRET_KEY = 'Sofwtare-engineering-secret-key'
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
