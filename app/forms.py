13a9773387bdd40133cae63643efdf1f6ed7e695
Adam Brown <sc20asb@leeds.ac.uk>
2022-02-16 15:19:59 +0000

    Fixed errors in forms, views and models. Also started basic home page, but need base.html to be finsihed before I can continue

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, DateField, TextAreaField, BooleanField, PasswordField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, EqualTo, Length, NumberRange, Email, InputRequired
-from .models import Hiring_place, Scooter, Hire_session, Employee, Guest_user,
-User, Card_Payment, Feedback
+from wtforms.validators import DataRequired, EqualTo, Length, NumberRange, InputRequired#Email, 
+from .models import Hiring_place, Scooter, Hire_session, Employee, Guest_user, User, Card_Payment, Feedback
 
 class LoginForm(FlaskForm):
-    email = StringField("email",validators=[DataRequired("Please enter an email address"),
-    Email("Please enter a valid email address")])
+    email = StringField("email")#,validators=[DataRequired("Please enter an email address"),Email("Please enter a valid email address")])
     password = PasswordField('password', validators=[DataRequired("Please enter your password")])
     rememberMe = BooleanField('Remember Me?') #Option for user to stay logged in or not
--- app/views.py
+++ app/views.py
@@ -1,8 +1,7 @@
 from app import app
 from flask import render_template
 from flask_login import current_user, login_user, login_required, logout_user
-from .models import Hiring_place, Scooter, Hire_session, Employee, Guest_user,
-User, Card_Payment, Feedback
+from .models import Hiring_place, Scooter, Hire_session, Employee, Guest_user, User, Card_Payment, Feedback
 from .forms import LoginForm
 
 @app.route('/')
@@ -30,9 +29,9 @@ def user_login():
     form = LoginForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            int foundUser = 1 #Will be decremented if user account cannot be found (if no
+            foundUser = 1 #Will be decremented if user account cannot be found (if no
             #users are found it will be automatically redirected)
-            int managerAccount = 0 #Checks if employee is a manager, increments if they are
+            managerAccount = 0 #Checks if employee is a manager, increments if they are
 
             findUser = User.query.filter_by(email=form.email.data).first()
 
@@ -40,15 +39,15 @@ def user_login():
                 flash("Invalid username or password","Error")
                 return redirect('/login')
 
-            else if findUser is None:
+            elif findUser is None:
                foundUser = 0
                findEmployee = Employee.query.filter_by(email_address=form.email.data).first()
 
                if findEmployee is None or not findEmployee.check_password(form.password.data):
                   flash("Invalid username or password","Error")
                   return redirect('/login')
-               
-               else if findEmployee.isManager == True:
+
+               elif findEmployee.isManager == True:
                      managerAccount = 1
 
             if foundUser == 0:
@@ -56,7 +55,7 @@ def user_login():
 
                if managerAccount == 1:
                   return redirect("/manager")
-                  
+
                else:
                   return redirect("/employee")
 
@@ -68,4 +67,3 @@ def user_login():
     return render_template('login.html',
                            title='Login page',
                            form=form)
-
