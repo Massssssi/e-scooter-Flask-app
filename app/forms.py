95b9d8087c0d6ec08d36e76c2bc61492b6c5547a
Ynyr Evans <sc20ye@leeds.ac.uk>
2022-02-16 13:24:34 +0000

    Added forms file

---
+++ app/forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, DateField, TextAreaField, BooleanField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length, NumberRange
+#from .models import 
