4b171ca11a00fe64906ac99a159bca6b549c17f5
Ynyr Evans <sc20ye@leeds.ac.uk>
2022-02-16 13:47:11 +0000

    Added login form to forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, DateField, TextAreaField, BooleanField, PasswordField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, EqualTo, Length, NumberRange
-#from .models import 
+from wtforms.validators import DataRequired, EqualTo, Length, NumberRange, Email, InputRequired
+from .models import Hiring_place, Scooter, Hire_session, Employee, Guest_user,
+User, Card_Payment, Feedback
+
+class LoginForm(FlaskForm):
+    email = StringField("email",validators=[DataRequired("Please enter an email address"),
+    Email("Please enter a valid email address")])
+    password = PasswordField('password', validators=[DataRequired("Please enter your password")])
+    rememberMe = BooleanField('Remember Me?') #Option for user to stay logged in or not
