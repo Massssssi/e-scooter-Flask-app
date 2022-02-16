0203e1073b784bd59e22a30db2e982a6f43b0fed
Ynyr Evans <sc20ye@leeds.ac.uk>
2022-02-16 16:01:10 +0000

    Added login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_login import LoginManager
+from flask_admin import Admin
+import os
+import logging
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 #Handles all migrations
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import views,models
 
