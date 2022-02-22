f756ee611410fd66809e7cf2d1a7e841aa0b8e2b
Chemseddine Benimoussa <sc20cwb@leeds.ac.uk>
2022-02-22 17:18:19 +0000

    Completed

--- app/views.py
+++ app/views.py
@@ -12,17 +12,14 @@ def main():
 def AddScooter():
     form  = scooterForm()
     form.location.choices = [(location.place_id, location.name) for location in models.Hiring_place.query.all()]
-
     if form.validate_on_submit():
         flash('Succesfully received from data. %s and %s'%(form.status.data, form.location.data))
 
         scooter = models.Scooter(status=form.status.data, location_id=form.location.data)
-        
+
         try:
             db.session.add(scooter)
             db.session.commit()
         except:
             flash('ERROR WHILE UPDATING THE SCOOTER TABLE')
-
-
-    return render_template('ScooterManagement.html', title = 'Add Scooter', form = form,)
+    return render_template('ScooterManagement.html', title = 'Add Scooter', form = form)
